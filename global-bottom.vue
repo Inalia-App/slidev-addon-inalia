f7add0cc722abb595c4f4eef174ee4c075afe98b
Estéban <e.soubiran25@gmail.com>
2025-08-19 19:13:22 +0200

    feat: show a banner to run the talk when needed (#23)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,9 +1,14 @@
 <script lang="ts" setup>
 import InaliaAudienceQuestionHighlighted from './components/InaliaAudienceQuestionHighlighted.vue'
 import InaliaLiveReactions from './components/InaliaLiveReactions.vue'
+import InaliaRunToContinue from './components/InaliaRunToContinue.vue'
+import { useInaliaTalk } from './composables/useInaliaTalk'
+
+const { talk, run } = useInaliaTalk()
 </script>
 
 <template>
   <InaliaLiveReactions class="w-full h-full relative" />
   <InaliaAudienceQuestionHighlighted class="absolute inset-0" />
+  <InaliaRunToContinue v-if="talk?.slidev.run_to_continue" class="absolute z-10 bottom-0 inset-x-0" @run="run" />
 </template>
--- utils/api.ts
+++ utils/api.ts
@@ -17,6 +17,12 @@ export async function fetchTalk(): Promise<Talk> {
   return api<{ data: Talk }>(`/api/${import.meta.env.VITE_INALIA_USERNAME}/talks/${import.meta.env.VITE_INALIA_TALK_NUMBER}`).then(response => response.data)
 }
 
+export async function runTalk(): Promise<void> {
+  return api(`/api/${import.meta.env.VITE_INALIA_USERNAME}/talks/${import.meta.env.VITE_INALIA_TALK_NUMBER}/run`, {
+    method: 'POST',
+  }).then(response => response.data)
+}
+
 export async function fetchQuestion(questionNumber: number): Promise<Question> {
   return api<{ data: Question }>(`/api/${import.meta.env.VITE_INALIA_USERNAME}/talks/${import.meta.env.VITE_INALIA_TALK_NUMBER}/questions/${questionNumber}`).then(response => response.data)
 }
