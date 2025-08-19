ccf8a46632d8fd209d2c13b289341c32fbbdfbc1
barbapapazes <e.soubiran25@gmail.com>
2025-08-19 20:41:12 +0200

    chore: remove unused api

--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,3 @@
-import type { AudienceQuestion } from '../types/audience-question'
-import type { FeedbackMessage } from '../types/feedback-message'
-import type { FeedbackReaction } from '../types/feedback-reaction'
 import type { Question } from '../types/question'
 import type { Talk } from '../types/talk'
 import { ofetch } from 'ofetch'
@@ -25,16 +22,4 @@ export async function runTalk(): Promise<void> {
 
 export async function fetchQuestion(questionNumber: number): Promise<Question> {
   return api<{ data: Question }>(`/api/${import.meta.env.VITE_INALIA_USERNAME}/talks/${import.meta.env.VITE_INALIA_TALK_NUMBER}/questions/${questionNumber}`).then(response => response.data)
-}
-
-export async function fetchAudienceQuestions(): Promise<AudienceQuestion[]> {
-  return api<{ data: AudienceQuestion[] }>(`/api/${import.meta.env.VITE_INALIA_USERNAME}/talks/${import.meta.env.VITE_INALIA_TALK_NUMBER}/audience-questions`).then(response => response.data)
-}
-
-export async function fetchFeedbackMessages(): Promise<FeedbackMessage[]> {
-  return api<{ data: FeedbackMessage[] }>(`/api/${import.meta.env.VITE_INALIA_USERNAME}/talks/${import.meta.env.VITE_INALIA_TALK_NUMBER}/feedback-messages`).then(response => response.data)
-}
-
-export async function fetchFeedbackReactions(): Promise<FeedbackReaction[]> {
-  return api<{ data: FeedbackReaction[] }>(`/api/${import.meta.env.VITE_INALIA_USERNAME}/talks/${import.meta.env.VITE_INALIA_TALK_NUMBER}/feedback-reactions`, {}).then(response => response.data)
 }
