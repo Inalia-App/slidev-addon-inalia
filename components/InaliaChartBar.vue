0e822ff5ccfbf89f125043acdf9a7d5131bb86c7
barbapapazes <e.soubiran25@gmail.com>
2025-09-08 23:20:16 +0200

    fix: remove dependency to virtual module

--- components/InaliaChartBar.vue
+++ components/InaliaChartBar.vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
 import type { DeepReadonly, PropType } from 'vue'
 import type { SelectData } from '../types/data'
-import { useSlideContext } from '@slidev/client'
 import { VisStackedBar, VisXYContainer } from '@unovis/vue'
 
 defineProps({
@@ -11,26 +10,9 @@ defineProps({
   },
 })
 
-const { $slidev, $frontmatter } = useSlideContext()
-const inaliaConfig = $slidev.configs.inalia
-
-const width = $frontmatter.inalia?.bar?.width
-const height = $frontmatter.inalia?.bar?.height
-
-const barWidth
-  = $frontmatter.inalia?.bar?.barWidth
-    ?? inaliaConfig?.bar?.barWidth
-    ?? 30
-
-const barPadding
-  = $frontmatter.inalia?.bar?.barPadding
-    ?? inaliaConfig?.bar?.barPadding
-    ?? 0.1
-
-const roundedCorners
-  = $frontmatter.inalia?.bar?.roundedCorners
-    ?? inaliaConfig?.bar?.roundedCorners
-    ?? 4
+const barWidth = 30
+const barPadding = 0.1
+const roundedCorners = 4
 
 const x = (d: SelectData[number], index: number) => index
 const y = (d: SelectData[number]) => d.count
@@ -38,7 +20,7 @@ const color = (d: SelectData[number]) => d.color
 </script>
 
 <template>
-  <VisXYContainer :data="data" :width="width" :height="height">
+  <VisXYContainer :data="data">
     <VisStackedBar
       :x="x"
       :y="y"
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,8 +3,7 @@ import vue from 'unplugin-vue/rolldown'
 
 export default defineConfig({
   entry: ['./index.ts'],
-  platform: 'neutral',
+  platform: 'browser',
   plugins: [vue({ isProduction: true })],
   dts: { vue: true },
-  external: ['#slidev/configs', '#slidev/slides', 'server-reactive:nav', 'server-reactive:drawings?diff', '@vue/devtools-api'],
 })
